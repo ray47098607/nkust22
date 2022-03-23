d7e1808b26fd499690afc6178e8756dbda33d816
ray47098607 <64784033+ray47098607@users.noreply.github.com>
2022-03-23 11:39:47 +0800

    Update 4.27.c

--- exe/220316/HW/4.27.c
+++ exe/220316/HW/4.27.c
@@ -5,12 +5,12 @@ int main()
     int a, b, c, count=0;
     printf("500以内的畢氏三元數有：\n");
     printf("  a     b    c      \n");
-    /*求100以内勾股数*/
+  
     for(a=1; a<=500; a++)
         for(b=a+1; b<=500; b++)
         {
-            c=(int)sqrt(a*a+b*b);  /*求c值*/
-            if(c*c==a*a+b*b && a+b>c && a+c>b && b+c>a && c<=500)  /*判断c的平方是否等于a2+b2*/
+            c=(int)sqrt(a*a+b*b);  
+            if(c*c==a*a+b*b && a+b>c && a+c>b && b+c>a && c<=500)  
             {
                 printf("%4d %4d %4d     ", a, b, c);
                 count++;
